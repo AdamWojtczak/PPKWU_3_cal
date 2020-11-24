05a35f1802bfe5b4eebe6aed4ae3449c6244a842
AdamWojtczak <adamwojtczak807@gmail.com>
2020-11-24 14:23:11 +0100

    Added ical4j jar and repaired calendar scrapping

--- src/main/java/com/example/demo/URLReader.java
+++ src/main/java/com/example/demo/URLReader.java
@@ -1,4 +1,14 @@
 package com.example.demo;
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Date;
+import net.fortuna.ical4j.model.Property;
+import net.fortuna.ical4j.model.component.VEvent;
+import net.fortuna.ical4j.model.parameter.Value;
+import net.fortuna.ical4j.model.property.CalScale;
+import net.fortuna.ical4j.model.property.ProdId;
+import net.fortuna.ical4j.model.property.Version;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -8,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.net.*;
 import java.io.*;
 
@@ -42,16 +53,102 @@ public class URLReader {
 	}
 
 	@RequestMapping(method = RequestMethod.GET, path = "/events")
-	public static String getMonthEvents() throws Exception {
+	public static String getMonthEvents(@RequestParam("year") String year, @RequestParam("month") String month) throws Exception {
 
-		Document doc = Jsoup.connect("http://localhost:8080/?year=2020&month=10").get();
+		if (month.length() == 1) {
+			if(!month.startsWith("0")) {
+				month = "0" + month;
+			}
+		}
+		StringBuilder urlBuilder = new StringBuilder();
+		urlBuilder.append("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=");
+		urlBuilder.append(year);
+		urlBuilder.append("&miesiac=");
+		urlBuilder.append(month);
+		urlBuilder.append("&lang=01");
+
+		Document doc = Jsoup.connect(urlBuilder.toString()).get();
 		Elements newsHeadlines = doc.select(".active");
 
 		StringBuilder str = new StringBuilder();
 		for (Element headline : newsHeadlines) {
-			str.append(headline.toString());
+			if(headline.attr("class").equals("active")) {
+				String date = headline.text();
+				Elements title = headline.getElementsByTag("p");
+				String href = headline.attr("href");
+				if( !href.contains("javascript:void();")) {
+					str.append(date + ": " + title.text() + " " + href + "\n");
+				}
+			}
 		}
+
 		System.out.println("aaaaaaaaaaaaa");
 		return str.toString();
 	}
+
+	@RequestMapping(method = RequestMethod.GET, path = "/cal")
+	public static String getCal(@RequestParam("year") String year, @RequestParam("month") String month, HttpServletResponse response) throws Exception {
+
+		if (month.length() == 1) {
+			if(!month.startsWith("0")) {
+				month = "0" + month;
+			}
+		}
+		StringBuilder urlBuilder = new StringBuilder();
+		urlBuilder.append("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=");
+		urlBuilder.append(year);
+		urlBuilder.append("&miesiac=");
+		urlBuilder.append(month);
+		urlBuilder.append("&lang=01");
+
+		Document doc = Jsoup.connect(urlBuilder.toString()).get();
+		Elements newsHeadlines = doc.select(".active");
+
+		StringBuilder str = new StringBuilder();
+		for (Element headline : newsHeadlines) {
+			if(headline.attr("class").equals("active")) {
+				String date = headline.text();
+				Elements title = headline.getElementsByTag("p");
+				String href = headline.attr("href");
+				if( !href.contains("javascript:void();")) {
+					str.append(date + ": " + title.text() + " " + href + "\n");
+				}
+			}
+		}
+
+
+
+		FileInputStream fin = new FileInputStream("mycalendar.ics");
+
+		CalendarBuilder builder = new CalendarBuilder();
+
+		Calendar calendar = builder.build(fin);
+
+		calendar.getProperties().add(new ProdId("-//AdamWojtczak//iCal4j 1.0/PL"));
+		calendar.getProperties().add(Version.VERSION_2_0);
+		calendar.getProperties().add(CalScale.GREGORIAN);
+
+
+
+		try {
+			InputStream inputStream = fin;
+			response.setContentType("text/calendar;charset=utf-8");
+			IOUtils.copy(inputStream, response.getOutputStream());
+			response.flushBuffer();
+		} catch (FileNotFoundException fileNotFoundException) {
+			fileNotFoundException.printStackTrace();
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+
+		System.out.println("aaaaaaaaaaaaa");
+		return str.toString();
+	}
+
+	class ICalEvent {
+		int day;
+		String title;
+		String eventURL;
+	}
+
 }

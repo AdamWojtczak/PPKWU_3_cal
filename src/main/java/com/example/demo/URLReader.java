1f945f4d3c46af73e843b19e09e6b8a688e6cdeb
AdamWojtczak <adamwojtczak807@gmail.com>
2020-11-24 15:10:03 +0100

    Getting through the calendar ready

--- src/main/java/com/example/demo/URLReader.java
+++ src/main/java/com/example/demo/URLReader.java
@@ -5,9 +5,8 @@ import net.fortuna.ical4j.model.Date;
 import net.fortuna.ical4j.model.Property;
 import net.fortuna.ical4j.model.component.VEvent;
 import net.fortuna.ical4j.model.parameter.Value;
-import net.fortuna.ical4j.model.property.CalScale;
-import net.fortuna.ical4j.model.property.ProdId;
-import net.fortuna.ical4j.model.property.Version;
+import net.fortuna.ical4j.model.property.*;
+import net.fortuna.ical4j.util.UidGenerator;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -21,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class URLReader {
@@ -102,7 +103,9 @@ public class URLReader {
 		urlBuilder.append("&lang=01");
 
 		Document doc = Jsoup.connect(urlBuilder.toString()).get();
-		Elements newsHeadlines = doc.select(".active");
+		Elements newsHeadlines = doc.select("td");
+
+		List<ICalEvent> iCalEventList = new ArrayList<ICalEvent>();
 
 		StringBuilder str = new StringBuilder();
 		for (Element headline : newsHeadlines) {
@@ -110,28 +113,50 @@ public class URLReader {
 				String date = headline.text();
 				Elements title = headline.getElementsByTag("p");
 				String href = headline.attr("href");
-				if( !href.contains("javascript:void();")) {
-					str.append(date + ": " + title.text() + " " + href + "\n");
+				if(date.charAt(0) > '0' && date.charAt(0) < '9' && date.charAt(1) > '0' && date.charAt(1) < '9') {
+					date = "0" + date;
 				}
+				String titleString = "";
+				if(date.length() > 2 ) {
+					titleString = date.substring(2);
+					date = date.substring(0,2);
+				}
+
+				if( !href.contains("javascript:void();")) {
+					str.append(date + ": " + title + " " + href + "\n");
+				}
+				if(!titleString.isEmpty()) {
+					titleString = title.text();
+				}
+				iCalEventList.add(new ICalEvent(new Date(year.toString() + month.toString() + date.toString()), titleString, href));
 			}
 		}
 
 
 
-		FileInputStream fin = new FileInputStream("mycalendar.ics");
+		File fin = new File("mycalendar.ics");
 
-		CalendarBuilder builder = new CalendarBuilder();
 
-		Calendar calendar = builder.build(fin);
+
+		Calendar calendar = new Calendar();
 
 		calendar.getProperties().add(new ProdId("-//AdamWojtczak//iCal4j 1.0/PL"));
 		calendar.getProperties().add(Version.VERSION_2_0);
 		calendar.getProperties().add(CalScale.GREGORIAN);
 
+		UidGenerator ug = () -> new Uid("adres.synchronizacji@example.com");
 
+		for(ICalEvent iCalEvent : iCalEventList) {
+			VEvent vEvent = new VEvent(iCalEvent.day,iCalEvent.title);
+			vEvent.getProperties().add(ug.generateUid());
+			Url url = new Url();
+			url.setValue(iCalEvent.eventURL);
+			vEvent.getProperties().add(url);
+			calendar.getComponents().add(vEvent);
+		}
 
 		try {
-			InputStream inputStream = fin;
+			InputStream inputStream = new FileInputStream(fileWriter);
 			response.setContentType("text/calendar;charset=utf-8");
 			IOUtils.copy(inputStream, response.getOutputStream());
 			response.flushBuffer();
@@ -145,10 +170,16 @@ public class URLReader {
 		return str.toString();
 	}
 
-	class ICalEvent {
-		int day;
+	static class ICalEvent {
+		Date day;
 		String title;
 		String eventURL;
+
+		public ICalEvent(Date day, String title, String eventURL) {
+			this.day = day;
+			this.title = title;
+			this.eventURL = eventURL;
+		}
 	}
 
 }
